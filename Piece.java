c2ecf4fa4a283a8e2b4d695811446fc6bebd0f2a
Rayanb <rayanbouts@gmail.com>
2016-03-07 16:36:21 +0000

    iterate through all points to have a list of possible moves

--- Piece.java
+++ Piece.java
@@ -1,12 +1,41 @@
-
 public class Piece{
-    public Point position;
-    public Board board;
+    public int position;
+    public String player;
+    public ArrayList possibleMoves;
 
-    public Piece(Point position, Board board) {
+    public Piece(int position) {
         this.position = position;
         this.board = board;
+        this.possibleMoves = calculatePossibleMoves();
+    }
+
+    public static ArrayList calculatePossibleMoves(Board board){
+      ArrayList moves = new ArrayList;
+      for(point : board.points){
+        if(point.canBeAttacked == true && !point.player.equals(this.player) && respectsDirection(point.position)){
+          moves.add(point.position);
+        }
+      }
+      return moves;
+    }
+
+    public static boolean respectsDirection(int newPosition){
+      if(this.player == "player1" && this.position < newPosition){
+        return true;
+      }
+      else if(this.player == "player2" && this.position > newPosition){
+        return true;
+      }
+      else{
+        return false;
+      }
     }
 
 
 }
+
+
+position : 1
+
+player 1 : 2, 3, 4 ....
+player 2 : 0
