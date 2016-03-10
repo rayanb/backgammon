4c0e94cc502a2b9c0fada1c3f4bf48f63f578238
Rayanb <rayanbouts@gmail.com>
2016-03-10 16:44:37 +0000

    debugged population of board

--- Board.java
+++ Board.java
@@ -1,11 +1,52 @@
-public class Board {
-    public Point[] points = new Point[26];
-    // points 0 and 25 designated as the bar
-    // makes sense, as pieces transferred off the bar "move" based on the die roll
-    // with a target point equal to the die roll
+import java.util.Arrays;
 
-    // for pieces trying to bear off, needs a special check to move to 0/25
-    // and a way to avoid Array Index Out Of Bounds exception
+// points 0 and 25 designated as the bar
+// makes sense, as pieces transferred off the bar "move" based on the die roll
+// with a target point equal to the die roll
+// for pieces trying to bear off, needs a special check to move to 0/25
+// and a way to avoid Array Index Out Of Bounds exception
+
+public class Board {
+    public Point[] points;
+
+    public Board(){
+      this.points  = new Point[26];
+      Arrays.fill(this.points, new Point(this));
+    }
+
+    public static void main(String[] args){
+      Board hey = new Board();
+      hey.populatePoints();
+    }
+
+    public void populatePoints(){
+      int index = 0;
+      for(Point point : this.points){
+        point.position = index;
+        switch(index){
+          case 1:  point.populate("player1", 2);
+                   break;
+          case 5:  point.populate("player2", 5);
+                   break;
+          case 7:  point.populate("player2", 3);
+                   break;
+          case 12: point.populate("player1", 5);
+                   break;
+          case 13: point.populate("player2", 5);
+                   break;
+          case 17: point.populate("player1", 3);
+                   break;
+          case 19: point.populate("player1", 5);
+                   break;
+          case 24: point.populate("player2", 2);
+                   break;
+          default: point.populate(null, 0);
+                   break;
+        }
+        point.isVulnerable();
+        index++;
+      }
+    }
 
 }
 
--- Point.java
+++ Point.java
@@ -1,25 +1,35 @@
 import java.util.ArrayList;
 
 public class Point {
-    public Board board;
-    public boolean isVulnerable;
-    public String player;
-    public ArrayList<Piece> pieces;
-    public int position;
+  public Board board;
+  public boolean isVulnerable;
+  public String player;
+  public ArrayList<Piece> pieces;
+  public int position;
 
-    public Point(ArrayList<Piece> startingPieces, int position, Board board) {
+  public Point(Board board) {
+    this.board = board;
+    this.isVulnerable = false;
+    this.pieces = new ArrayList<Piece>();
+  }
 
+  public boolean isVulnerable() {
+    if (this.pieces.size() > 1)
+      return setIsVulnerable(false);
+    else
+      return setIsVulnerable(true);
+  }
+
+  public boolean setIsVulnerable(boolean isVulnerable) {
+    this.isVulnerable = isVulnerable;
+    return this.isVulnerable;
+  }
+
+  public void populate(String player, int numOfPieces){
+    for(int i = 0; i< numOfPieces; i++){
+      Piece newPiece = new Piece(this,player);
+      this.pieces.add(newPiece);
     }
+  }
 
-    public boolean isVulnerable() {
-        if (pieces.size() > 1)
-            return setIsVulnerable(false);
-        else
-            return setIsVulnerable(true);
-    }
-
-    public boolean setIsVulnerable(boolean isVulnerable) {
-        this.isVulnerable = isVulnerable;
-        return this.isVulnerable;
-    }
 }
